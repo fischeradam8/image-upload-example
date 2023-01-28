7423bffddc8dab25f1ab0defc3d9526d36c4265b
adam <adam@stylehub.com>
2023-01-28 08:51:50 +0100

    add plus icon

--- client/src/components/atoms/icons.ts
+++ client/src/components/atoms/icons.ts
@@ -1,2 +1,3 @@
 export { ReactComponent as LimeIcon } from "../icons/lime.svg";
 export { ReactComponent as LinkIcon } from "../icons/link.svg";
+export { ReactComponent as PlusIcon } from "../icons/plus.svg";
