ef04062ef20269652f5c3ec0be44fef204daed25
adam <adam@stylehub.com>
2023-01-28 13:12:13 +0100

    add basic mongoose implementation

--- server/app.ts
+++ server/app.ts
@@ -1,46 +1,49 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import multer from "multer";
+import bodyParser from "body-parser";
+import { Schema, model, connect } from "mongoose";
 
 const app: Express = express();
 const port = "3001";
+const upload = multer();
 
 app.use(cors());
+app.use(bodyParser.json());
+
+interface ImageInterface {
+  image: Buffer;
+  description?: string;
+}
+
+const imageSchema = new Schema<ImageInterface>({
+  image: { type: Buffer, required: true },
+  description: { type: String, required: false },
+});
+
+const ImageModel = model<ImageInterface>("Image", imageSchema);
 
 //LIST
-app.get("/images", (req: Request, res: Response) => {
-  res.send([
-    {
-      id: "1",
-      name: "placeholder1",
-      size: "1.21MB",
-      width: "1280",
-      height: "1200",
-      uploadedAt: "2023-01-24",
-      uploadedFrom: "192.168.1.1",
-      description: "Lorem ipsum...",
-    },
-    {
-      id: "2",
-      name: "placeholder2",
-      size: "1.22MB",
-      width: "1280",
-      height: "1200",
-      uploadedAt: "2023-01-24",
-      uploadedFrom: "192.168.1.1",
-      description: "Lorem ipsum 2...",
-    },
-    {
-      id: "3",
-      name: "placeholder3",
-      size: "1.23MB",
-      width: "1280",
-      height: "1200",
-      uploadedAt: "2023-01-24",
-      uploadedFrom: "192.168.1.1",
-      description: "Lorem ipsum 3...",
-    },
-  ]);
+app.get("/images", (request: Request, response: Response) => {
+  connect("mongodb://127.0.0.1:27017/images-test").then(() => {
+    ImageModel.find().then((data) => response.send(data));
+  });
 });
+
+app.post(
+  "/images",
+  upload.single("file"),
+  (request: Request, response: Response) => {
+    connect("mongodb://127.0.0.1:27017/images-test").then(() => {
+      const document = new ImageModel({
+        image: request.body.file,
+        description: request.body.description,
+      });
+      document.save().then(() => console.log(document));
+      response.send("OK");
+    });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Listening on Port 3001`);
